f5f16c992f455dff54a050457927fec10c23ebcd
John Azzaro <jazzaro@temple.edu>
2020-03-24 22:36:29 -0700

    imporved test comp, additional workflow

--- jest-test/src/leapyearprogram/index.test.js
+++ jest-test/src/leapyearprogram/index.test.js
@@ -1,6 +1,6 @@
 import leapYear  from './index';
 
-it('should NOT be a leap year if divisible by 100, not 400', function() {
+it('should NOT be a leap year if divisible by 100, not 400', () => {
   const input = 1900;
   const expectedOutput = false; 
   expect(leapYear(input)).toBe(expectedOutput);
