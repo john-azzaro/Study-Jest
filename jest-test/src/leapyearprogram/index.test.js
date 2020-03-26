f141f249e0fa487e59de45e3b38079c9f1841968
John Azzaro <jazzaro@temple.edu>
2020-03-25 18:10:37 -0700

    simplified tests

--- jest-test/src/leapyearprogram/index.test.js
+++ jest-test/src/leapyearprogram/index.test.js
@@ -1,14 +1,41 @@
+// import leapYear  from './index';
+
+// it('should NOT be a leap year if divisible by 100, not 400', function() {
+//   const input = 1900;
+//   const expectedOutput = false; 
+//   expect(leapYear(input)).toBe(expectedOutput);
+// });
+
+// it('should be a leap year if divisible by 4, not 100', function() {
+//   const input = 1984;
+//   const expectedOutput = true;
+//   const actualOutput = leapYear(input);
+//   expect(actualOutput).toBe(expectedOutput);
+// });
+
+
+
+
 import leapYear  from './index';
 
 it('should NOT be a leap year if divisible by 100, not 400', function() {
-  const input = 1900;
-  const expectedOutput = false; 
-  expect(leapYear(input)).toBe(expectedOutput);
+  expect(leapYear(1900)).toBe(false);
 });
 
 it('should be a leap year if divisible by 4, not 100', function() {
-  const input = 1984;
-  const expectedOutput = true;
-  const actualOutput = leapYear(input);
-  expect(actualOutput).toBe(expectedOutput);
+  expect(leapYear(1984)).toBe(true);
+});
+
+it('should NOT be a leap year if not divisible by 4', function() {
+  expect(leapYear(1983)).toBe(false);
+});
+
+it('should be a leap year if divisible by 400', function() {
+  expect(leapYear(2000)).toBe(true);
+});
+
+it('should throw an error for years before 1582', function() {
+  expect(function() {
+    leapYear(1568);
+  }).toThrow();
 });
