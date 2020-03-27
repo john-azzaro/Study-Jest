d040c407164a807b54225714b1cd6d8af97cf86e
John Azzaro <jazzaro@temple.edu>
2020-03-26 21:59:33 -0700

    updated app added css

--- snapshot-smoke-test/src/App.js
+++ snapshot-smoke-test/src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>YOUR APPLICATION NAME!</h1>
+        <h1>Snapshot and Smoke Test!</h1>
         <Messages name="Messages" unread={0}/>
         <Messages name="Notifications" unread={10}/>
       </div>
