2576abe3dbab54381a8deab33bda3f9cf97b3d61
John Azzaro <jazzaro@temple.edu>
2020-03-26 22:03:19 -0700

    updated

--- snapshot-smoke-test/src/messages.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Messages from './Messages';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Messages />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
