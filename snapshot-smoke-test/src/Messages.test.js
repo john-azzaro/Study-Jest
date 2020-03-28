54b77774e3a80b257ae994b8d379f95839a8b499
John Azzaro <jazzaro@temple.edu>
2020-03-27 18:27:05 -0700

    snapshot setup

--- snapshot-smoke-test/src/Messages.test.js
+++ snapshot-smoke-test/src/Messages.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Messages from './Messages';
+import renderer from 'react-test-renderer';
 
 it('renders without crashing', function() {
   const div = document.createElement('div');
   ReactDOM.render(<Messages />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('renders the UI as expected', function() {
+  const tree = renderer
+    .create(<Messages name="Messages" unread={4}/>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();  
+  });
