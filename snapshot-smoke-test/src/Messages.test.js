ac7ea7b77e8d88dc62abafbe8ccb8687ecc0b370
John Azzaro <jazzaro@temple.edu>
2020-03-27 18:07:59 -0700

    workflow additions, test changes

--- snapshot-smoke-test/src/Messages.test.js
+++ snapshot-smoke-test/src/Messages.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Messages from './Messages';
 
-it('renders without crashing', () => {
+it('renders without crashing', function() {
   const div = document.createElement('div');
   ReactDOM.render(<Messages />, div);
   ReactDOM.unmountComponentAtNode(div);
